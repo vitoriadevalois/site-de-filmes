7cbf390c71a6766dcdfeca177729f4fcbe0f289c
Vitória de Valois <81873935+vitoriadevalois@users.noreply.github.com>
2021-09-23 18:58:06 -0300

    update

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
-arrows.forEach((arrow, index) => {
-        const itemNumber = movieLists[index].querySelectorAll("img").length;
-        let clickCounter = 0;
-    arrow.addEventListener("click", () => {
-        clickCounter++;
-        if(itemNumber - (4 + clickCounter) > 0){
-           movieLists[index].style.transform = `translateX(${
-            movieLists[index].computedStyleMap().get("transform")[0].x.value
-        -300}px)`; 
-        } else{
-            movieLists[index].style.transform = "transflateX(0)"
-        }
-    });
-
-    console.log(movieLists[index].querySelectorAll("img").length)
-});
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
